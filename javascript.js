18cbffb89bf74978d2b869013146d0c999e8a4b6
Andrew Akers <andrew@akrs.me>
2014-02-24 23:46:52 -0800

    Seperated js and HTML added code for communication

---
+++ javascript.js
@@ -0,0 +1,18 @@
+var options = {};
+var reversedDropdown = document.getElementById("chooser");
+
+var load = function () {
+    options = JSON.parse(decodeURIComponent(window.location.href.split("?")[1]));
+    reversedDropdown.selectedIndex = options.reversed;
+};
+
+var encodeAndClose = function () {
+    if (reversedDropdown.options[reversedDropdown.selectedIndex].value == "reversed") {
+        options.reversed = 1;
+    }
+    alert("WIN");
+    window.location.href = "pebblejs://close#" + encodeURIComponent(JSON.stringify(options));
+};
+
+document.getElementById("close").onclick = encodeAndClose;
+window.onload = load;
